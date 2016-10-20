13db1c3b7235fed4b3046f6ff10b1b2ad7a046cd
Felipe Barbalho <felipebarbalho.95@gmail.com>
2016-10-20 04:05:55 -0200

    Update2

--- quadrado4/src/quadrado4/M.java
+++ quadrado4/src/quadrado4/M.java
@@ -10,6 +10,28 @@ public class M {
 		
 	}
 	
+	public M(int [][]m) {
+		this.m = new int[m.length][m.length];
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m.length; j++) {
+				this.m[i][j] = m[i][j];
+			} 
+		}
+	}
+	
+	public void print(){
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m.length; j++) {
+				if(m[i][j]>9)
+					System.out.print(m[i][j]+" ");
+				else{
+					System.out.print(m[i][j]+"  ");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
 	public int get(int i, int j){
 		return m[i][j];
 	}
--- quadrado4/src/quadrado4/QuadradoMagicoNormal.java
+++ quadrado4/src/quadrado4/QuadradoMagicoNormal.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package quadrado4;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author barbalho12
- */
+
 public class QuadradoMagicoNormal {
 
     public static void main(String[] args) {
@@ -21,14 +13,11 @@ public class QuadradoMagicoNormal {
 		
 		int [][] m = new int[4][4];
 		int [] a = new int[8];
-		int cont=0;
- 		boolean l1,l2,l3,l4,c1,c2,c3,c4,d1,d2;
- 		
- 		List<M> quadrados_magicos = new ArrayList<M>();
+
+ 		List<M> qm = new ArrayList<M>();
  		
  		int [] vetor = new int[16];
 		
-		//Iteração TOTAL
 		for (a[0] = 1; a[0] <= 16; a[0]++) {
 	    	for (a[1] = 1; a[1] <= 16; a[1]++) {
 			if(a[1]==a[0]) continue;
@@ -66,7 +55,6 @@ public class QuadradoMagicoNormal {
 			m[3][2] = a[6];
 			m[3][3] = a[7];
 			
-			
 			if(!(m[0][0]<=16 && m[0][0]>0 &&
 				m[0][1]<=16 && m[0][1]>0 &&
 				m[0][2]<=16 && m[0][2]>0 &&
@@ -77,114 +65,99 @@ public class QuadradoMagicoNormal {
 				m[2][0]<=16 && m[2][0]>0)){
 				continue;
 				
-				
-				
 			}else{
+				
 				for (int i = 0; i < 16; i++) {
 					vetor[i]= -1;
 				}
+				
 				boolean repetido = false;
+				
 				for (int i = 0, k=0; i < 4; i++) {
 					for (int j = 0; j < 4; j++, k++) {
 						vetor[m[i][j]-1]= 1;
 					}
 				}
 				for (int i = 0; i < 16; i++) {
-					
 					if(vetor[i]==-1){
-						
 						repetido = true;
 						break;
 					}
 				}
 				
-				if(repetido) continue;
-				
-//				boolean rodadoHor = true;
-//				boolean rodadoAnHor = false;
-//				for (int i = 0; i < 4; i++) {
-//					for (int j = 0; j < 4; j++) {
-//						if(m[(4-j)-1][i] != m[i][j]){
-//							rodadoHor = false;
-//							break;
-//						}
-//					}
-//					if(rodadoHor) break;
-//				}
-//				for (int i = 0; i < 4; i++) {
-//					for (int j = 0; j < 4; j++) {
-//						if(m[(4-j)-1][i] != m[j][i]){
-//							rodadoAnHor = false;
-//							break;
-//						}
-//					}
-//					if(rodadoAnHor) break;
-//				}
-//				if(rodadoHor || rodadoAnHor) continue;
-//				
-				
-				
-//				
-//				if(m[3][0] == m[0][3])
-//				if(m[2][0] == m[1][3])
-//				if(m[1][0] == m[2][3])
-//				if(m[0][0] == m[3][3])
-				
-//				if(m[3][0] == m[0][0])
-//				if(m[2][0] == m[0][1])
-//				if(m[1][0] == m[0][2])
-//				if(m[0][0] == m[0][3])
-//				
-//				if(m[3][1] == m[1][0])
-//				if(m[2][1] == m[1][1])
-//				if(m[1][1] == m[1][2])
-//				if(m[0][1] == m[1][3])
-//				
-//				if(m[3][2] == m[2][0])
-//				if(m[2][2] == m[2][1])
-//				if(m[1][2] == m[2][2])
-//				if(m[0][2] == m[2][3])
-//				
-//				if(m[3][3] == m[3][0])
-//				if(m[2][3] == m[3][1])
-//				if(m[1][3] == m[3][2])
-//				if(m[0][3] == m[3][3])
-				
-//				//Linhas
-//	 			l1 = (m[0][0] + m[0][1] + m[0][2] + m[0][3]) == 34;
-//	 			l2 = (m[1][0] + m[1][1] + m[1][2] + a[0]) 	 == 34;
-//	 			l3 = (m[2][0] + a[1]    + a[2]    + a[3]) 	 == 34;
-//	 			l4 = (a[4]    + a[5]    + a[6]    + a[7]) 	 == 34;
-//				
-//				//Colunas
-//	 			c1 = (m[0][0] + m[1][0] + m[2][0] + a[4]) 	 == 34;
-//	 			c2 = (m[0][1] + m[1][1] + a[1]    + a[5]) 	 == 34;
-//	 			c3 = (m[0][2] + m[1][2] + a[2]    + a[6]) 	 == 34;
-//	 			c4 = (m[0][3] + a[0]    + a[3]    + a[7]) 	 == 34;
-//				
-//				//Diagonais
-//	 			d1 = (m[0][0] + m[1][1] + a[2]    + a[7]) 	 == 34;
-//	 			d2 = (m[0][3] + m[1][2] + a[1]    + a[4]) 	 == 34;
-//				
-//				
-//				if(l1 && l2 && l3 && l4 && c1 && c2 && c3 && c4 && d1 && d2){
-					
-						cont++;
-						//System.out.println(cont);
-		                System.out.println(m[0][0]+" "+m[0][1]+" "+m[0][2]+" "+m[0][3]+"\n"
-						+m[1][0]+" "+m[1][1]+" "+m[1][2]+" "+a[0]+"\n"
-						+m[2][0]+" "+a[1]+" "+a[2]+" "+a[3]+"\n"
-						+a[4]+" "+a[5]+" "+a[6]+" "+a[7]); 
-		                            System.out.println("-------"); 
-			
-					
-//				}
+				if(!repetido)
+					qm.add(new M(m));
 				
             }
 					
 			
 		}}}}}}}}
-		System.out.println(cont);
+		
+		
+		boolean [] removido = new boolean[qm.size()];
+		
+		for (int i = 0; i < removido.length; i++) {
+			removido[i] = false;
+		}
+		
+//		for (int h = 0; h < qm.size(); h++) {
+//			for (int l = 0; l < qm.size(); l++) {
+//				if(h==l || removido[h]) continue;
+//				
+//				boolean rodado1 = true;
+//				boolean rodado2 = true;
+//				boolean rodado3 = true;
+//				
+//				for (int i = 0; i < 4; i++) {
+//					for (int j = 0; j < 4; j++) {
+//						if(qm.get(h).get((4-j)-1,i) != qm.get(l).get(i,j)){
+//							rodado1 = false;
+//							break;
+//						}
+//					}
+//					if(rodado1) break;
+//				}
+//				for (int i = 0; i < 4; i++) {
+//					for (int j = 0; j < 4; j++) {
+//						
+//						if(qm.get(h).get((4-j)-1, i) != qm.get(l).get(j,(4-i)-1)){
+//							rodado2 = false;
+//							break;
+//						}
+//					}
+//					if(rodado2) break;
+//				}
+//				for (int i = 0; i < 4; i++) {
+//					for (int j = 0; j < 4; j++) {
+//					
+//						if(qm.get(h).get((4-j)-1, i) != qm.get(l).get((4-i)-1,(4-j)-1)){
+//							rodado3 = false;
+//							break;
+//						}
+//					}
+//					if(rodado3) break;
+//				}
+//				if(rodado1 || rodado2 || rodado3){
+//					removido[l] = true;
+//				}
+//			}
+//		}
+		
+
+		for (int i = removido.length-1; i >= 0; i--) {
+			if(removido[i]){
+				qm.remove(i);
+			}
+				
+		}
+			
+		
+		for (M mi : qm) {
+			mi.print();
+			System.out.println("---------");
+		}
+		
+		System.out.println(qm.size());
 		
 		elapsed = System.currentTimeMillis() - start;
 		System.out.println((elapsed/1000.0)+" segundos");
